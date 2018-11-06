af71d54b643eb3fe90277eddc446d5ec354aaec2
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-11-06 11:37:18 -0500

    with redux middleware test

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { logger } from "redux-logger";
 
 // internal modules
 import App from './components/app';
@@ -16,9 +17,12 @@ const reducers = combineReducers({
   activeCity: activeCityReducer
 });
 
+const middlewares = applyMiddleware(logger);
+
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <App />
   </Provider>,
   document.getElementById('root')
